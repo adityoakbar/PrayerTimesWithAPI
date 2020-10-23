4e80da759816c3aaacfed2964dff4a7299c97986
AdityoAkbar <38294809+adityoakbar@users.noreply.github.com>
2020-10-23 23:53:19 +0700

    Add files via upload

---
+++ Prayer Times/JS Prayer Times/app.js
@@ -0,0 +1,55 @@
+function prayerTimes(latitude,longitude) {
+    fetch('http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=4')
+    .then(response => response.json())
+    .then(function (response) {
+        let date    = new Date();
+        let today   = date.getDate() - 1;
+        let data    = response.data[0].timings;
+        
+        let app          = document.getElementById('app');
+        let table        = document.createElement('table');
+        let tableTbody   = document.createElement('tbody');
+        
+
+    for(i in data)
+        {
+            let row     = tableTbody.insertRow();
+            let name    = row.insertCell(0);
+            let time    = row.insertCell(1);
+                name.innerHTML = i ;
+                time.innerHTML = data[i];
+                tableTbody.appendChild(row);
+                
+        }
+        app.appendChild(table);
+        table.appendChild(tableTbody);
+        
+    });
+}
+
+function succes(position) {
+    prayerTimes(position.coords.latitude, position.coords.longitude);
+}
+
+function error() {
+    //Default Location Based Jakarta
+    prayerTimes('-6.200000','106.816666');
+}
+
+function userLocation() {
+    if (!navigator.geolocation) {
+        alert('Browser tidak mendukung Geolocation,Silahkan gunakan browser lain');
+    }else{
+        navigator.geolocation.getCurrentPosition(succes,error);
+    }
+}
+
+function index() {
+    let  app        = document.getElementById('app');
+    let  h3         = document.createElement('h3');
+    h3.innerHTML    = 'Prayer Times';
+
+    app.appendChild(h3);
+    userLocation();
+}
+index();
